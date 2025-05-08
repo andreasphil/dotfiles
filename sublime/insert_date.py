029dcab403f6642aed3ee89cfb8fe9b240204f2f
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2025-05-08 13:53:29 +0200

    Cleanup

--- sublime/insert_date.py
+++
@@ -1,9 +0,0 @@
-import sublime, sublime_plugin
-from datetime import datetime
-
-class InsertDateCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        current_date = datetime.now().strftime("%d.%m.%Y")
-
-        for sel in self.view.sel():
-            self.view.insert(edit, sel.begin(), current_date)
